6dab2b74c82a8eca5f5008fbe34558967f407d91
Benjamin Duppe <benjamin.duppe@dfki.de>
2024-02-19 14:53:17 +0100

    pipeline test 2

--- PipelineTest.py
+++ PipelineTest.py
@@ -9,7 +9,7 @@ def main():
         auto_version_bump=True,
         abort_on_failure=True,
         repo='https://github.com/Zarach/NeSy.git',
-        always_create_from_code=True
+        always_create_from_code=False
     )
 
     pipe.add_function_step(
